1c379a3e431a41e72f81191de179caaa89a1b808
jomariepolanco <jomariepolanco@gmail.com>
2020-11-03 11:50:27 -0500

    done

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,37 @@
 class CampersController < ApplicationController
+  before_action :set_camper, only: [:show]
+  
   def index
     @campers = Camper.all
   end
+
+  def show
+    render :show 
+  end
+
+  def new
+    @camper = Camper.new 
+    render :new 
+  end
+
+  def create
+    @camper = Camper.create(camper_params)
+    if @camper.valid?
+        redirect_to camper_path(@camper)
+    else
+      flash[:camper_errors] = @camper.errors.full_messages
+      redirect_to new_camper_path
+    end
+  end
+
+  private
+
+  def camper_params
+    params.require(:camper).permit(:name, :age)
+  end
+
+  def set_camper
+    @camper = Camper.find(params[:id])
+  end
+
 end
--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,2 +1,7 @@
 class Camper < ApplicationRecord
+    has_many :signups
+    has_many :activities, through: :signups 
+    
+    validates :name, uniqueness: {case_sensitive: false }
+    validates :age, numericality: {greater_than_or_equal_to: 8, less_than: 19}
 end
